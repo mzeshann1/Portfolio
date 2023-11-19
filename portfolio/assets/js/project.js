f50d23a55b456b23e561d4c847e19a192bd6b661
Muhammad Zeeshan <mzeshann1@gmail.com>
2023-11-19 06:21:36 +0500

    done

--- portfolio/assets/js/project.js
+++ portfolio/assets/js/project.js
@@ -1,20 +1,21 @@
-      // projects top mockup lightbox enable and disable
-      const lightboxTrigger = document.querySelector('.portfolio-lightbox-trigger');
-      const lightboxContainer = document.querySelector('.lightbox-container');
-      const lightboxImage = document.querySelector('.lightbox-image');
-      const closeLightbox = document.querySelector('.close-lightbox');
-      const lightboxOverlay = document.querySelector('.lightbox-overlay');
+// projects top mockup lightbox enable and disable
+const lightboxTrigger = document.querySelector('.portfolio-lightbox-trigger');
+const lightboxContainer = document.querySelector('.lightbox-container');
+const lightboxImage = document.querySelector('.lightbox-image');
+const closeLightbox = document.querySelector('.close-lightbox');
+const lightboxOverlay = document.querySelector('.lightbox-overlay');
 
-      lightboxTrigger.addEventListener('click', function (event) {
-          event.preventDefault();
-          const imageSource = this.getAttribute('data-image');
-          lightboxImage.src = imageSource;
-          lightboxContainer.style.display = 'flex';
-      });
+lightboxTrigger.addEventListener('click', function (event) {
+event.preventDefault();
+const imageSource = this.getAttribute('data-image');
+lightboxImage.src = imageSource;
+lightboxContainer.style.display = 'flex';
+});
 
-      closeLightbox.addEventListener('click', closeLightboxAndOverlay);
-      lightboxOverlay.addEventListener('click', closeLightboxAndOverlay);
+closeLightbox.addEventListener('click', closeLightboxAndOverlay);
+lightboxOverlay.addEventListener('click', closeLightboxAndOverlay);
 
-      function closeLightboxAndOverlay() {
-          lightboxContainer.style.display = 'none';
-      }
+function closeLightboxAndOverlay() {
+lightboxContainer.style.display = 'none';
+}
+
